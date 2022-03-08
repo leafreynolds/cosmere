094b5ec417b54c94b0b54b861ba6a079c576be47
Leaf <leafreynolds@gmail.com>
2022-03-08 21:31:48 +1300

    Fixed bronze allomancy glow effect

--- src/main/java/leaf/cosmere/mixin/EntityMixin.java
+++ src/main/java/leaf/cosmere/mixin/EntityMixin.java
@@ -7,6 +7,7 @@ package leaf.cosmere.mixin;
 import leaf.cosmere.cap.entity.SpiritwebCapability;
 import leaf.cosmere.constants.Manifestations;
 import leaf.cosmere.constants.Metals;
+import leaf.cosmere.manifestation.AManifestation;
 import leaf.cosmere.registry.ManifestationRegistry;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
@@ -20,7 +21,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Entity.class)
 public class EntityMixin
 {
-/*
     @Inject(method = "isGlowing", at = @At("RETURN"), cancellable = true)
     private void handleIsGlowing(CallbackInfoReturnable<Boolean> cir)
     {
@@ -36,7 +36,7 @@ public class EntityMixin
 
         SpiritwebCapability.get(clientPlayer).ifPresent(playerSpiritweb ->
         {
-            //if has bronze
+            //if the player does not have bronze, early exit
             if (!(playerSpiritweb.hasManifestation(Manifestations.ManifestationTypes.ALLOMANCY, Metals.MetalType.BRONZE.getID())))
             {
                 return;
@@ -44,16 +44,12 @@ public class EntityMixin
 
             SpiritwebCapability.get(target).ifPresent(targetSpiritweb ->
             {
-                //if has copper
+                //if target has copper and it's active, early exit
                 if (ManifestationRegistry.ALLOMANCY_POWERS.get(Metals.MetalType.COPPER).get().isActive(targetSpiritweb))
                 {
                     //check whether or not the target beats the player in allomantic power
-
-
-
                     return;
                 }
-
 
                 //get allomantic strength of
 
@@ -61,22 +57,36 @@ public class EntityMixin
 
                 //todo range to config
                 int range = 5 * mode;
+                boolean found = false;
+
+                //if target is player and has any active manifestations,
 
 
-                //get allomantic strength of copper
+                //or if target is not a player and has any manifestations at all
 
+                for (AManifestation manifestation : ManifestationRegistry.MANIFESTATION_REGISTRY.get())
+                {
+                    //don't tick powers that the user doesn't have
+                    //don't tick powers that are not active
+                    if (!(target instanceof PlayerEntity) && targetSpiritweb.hasManifestation(manifestation.getManifestationType(), manifestation.getPowerID()))
+                    {
+                        found = true;
+                        break;
+                    }
+                    else if ((target instanceof PlayerEntity) && manifestation.isActive(targetSpiritweb))
+                    {
+                        found = true;
+                        break;
+                    }
+                }
 
-                //if targets copper is on and their mode and strength is stronger than the player
-                //then close out
-
-
-                if (clientPlayer != null && clientPlayer.distanceTo(target) < range)
+                if ((clientPlayer != null && clientPlayer.distanceTo(target) < range) && found)
                 {
                     cir.setReturnValue(true);
                 }
             });
         });
-    }*/
+    }
 
 }
 
