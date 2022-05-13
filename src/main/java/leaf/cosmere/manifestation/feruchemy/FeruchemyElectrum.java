6de28df80a9836a7538104e687ae55cbe339573a
Leaf <leafreynolds@gmail.com>
2022-05-14 08:33:21 +1200

    Buff electrum feruchemy to be more noticeable

--- src/main/java/leaf/cosmere/manifestation/feruchemy/FeruchemyElectrum.java
+++ src/main/java/leaf/cosmere/manifestation/feruchemy/FeruchemyElectrum.java
@@ -23,14 +23,42 @@ public class FeruchemyElectrum extends FeruchemyBase
 		MobEffectInstance tapEffect = event.getEntityLiving().getEffect(EffectsRegistry.TAPPING_EFFECTS.get(getMetalType()).get());
 		MobEffectInstance storeEffect = event.getEntityLiving().getEffect(EffectsRegistry.STORING_EFFECTS.get(getMetalType()).get());
 
+		//take less damage when tapping
 		if (tapEffect != null && tapEffect.getDuration() > 0)
 		{
-			event.setAmount(event.getAmount() - ((tapEffect.getAmplifier() + 1) * 0.25f));
+			//always reduce damage by something
+			final int i = tapEffect.getAmplifier() + 1;
+
+			//never able to reduce by 100%
+			// 76% ish max? eg tap10 / 13 = 0.76
+			final float v = i / 13f;
+
+			// leaving 24%
+			// 1 - 0.76 = 0.24
+			final float v1 = 1 - v;
+
+			//eg 7 damage at tap 10 would be:
+			// 7 * 0.24 = 1.68 damage remaining
+			event.setAmount(event.getAmount() * v1);//todo convert to config
 		}
 
+		//take more damage when Storing
 		if (storeEffect != null && storeEffect.getDuration() > 0)
 		{
-			event.setAmount(event.getAmount() + ((storeEffect.getAmplifier() + 1) * 0.25f));
+			//always increase damage by something
+			final int i = storeEffect.getAmplifier() + 1;
+
+			//store 3 is the max so never able to increase damage to self by more than 23%?
+			// 23% ish max? eg store3 / 13 = 0.23
+			final float v = i / 13f;
+
+			// So we add 76% extra damage
+			// 1 + 0.23 = 1.23
+			final float v1 = 1 + v;
+
+			//eg 7 damage at store 3 would be:
+			// 7 * 1.23 = 8.61
+			event.setAmount(event.getAmount() * v1);//todo convert to config
 		}
 
 	}
