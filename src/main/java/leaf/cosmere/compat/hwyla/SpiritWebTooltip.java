341f3a6719ab433f9007a6faa45351f62242c007
Leaf <leafreynolds@gmail.com>
2022-06-06 17:59:26 +1200

    HWYLA limited to bronze allomancy and what's active

--- src/main/java/leaf/cosmere/compat/hwyla/SpiritWebTooltip.java
+++ src/main/java/leaf/cosmere/compat/hwyla/SpiritWebTooltip.java
@@ -8,6 +8,8 @@ import leaf.cosmere.cap.entity.SpiritwebCapability;
 import leaf.cosmere.constants.Manifestations;
 import leaf.cosmere.constants.Metals;
 import leaf.cosmere.manifestation.AManifestation;
+import leaf.cosmere.manifestation.allomancy.AllomancyBronze;
+import leaf.cosmere.registry.ManifestationRegistry;
 import leaf.cosmere.utils.helpers.TextHelper;
 import mcp.mobius.waila.api.EntityAccessor;
 import mcp.mobius.waila.api.IEntityComponentProvider;
@@ -23,39 +25,25 @@ public class SpiritWebTooltip implements IEntityComponentProvider
 	@Override
 	public void appendTooltip(ITooltip tooltip, EntityAccessor accessor, IPluginConfig iPluginConfig)
 	{
-/*
-        //todo add other ways we would allow the user to see spiritweb info
-        if (!accessor.getPlayer().isCreative())
-        {
-            return;
-        }*/
-
 		//check the entity we are trying to
-		SpiritwebCapability.get((LivingEntity) accessor.getEntity()).ifPresent(iSpiritweb ->
+		SpiritwebCapability.get(accessor.getPlayer()).ifPresent(clientPlayer ->
 		{
-
-			//todo check mistborn/feruchemist status
-			boolean mistborn = false;//iSpiritweb.isMistborn();
-			boolean fullFeruchemist = false;//iSpiritweb.isFullFeruchemist();
-			if (mistborn)
+			AllomancyBronze allomancyBronze = (AllomancyBronze) ManifestationRegistry.ALLOMANCY_POWERS.get(Metals.MetalType.BRONZE).get();
+			if (allomancyBronze.isMetalBurning(clientPlayer))
 			{
-				tooltip.add(TextHelper.createTranslatedText(Metals.MetalType.LERASIUM.getMistingName()));
-			}
-			if (fullFeruchemist)
-			{
-				tooltip.add(TextHelper.createTranslatedText(Metals.MetalType.LERASATIUM.getFerringName()));
-			}
-
-
-			//show all manifestations, including hemalurgic based ones.
-			for (AManifestation manifestation : iSpiritweb.getAvailableManifestations())
-			{
-				if ((mistborn && manifestation.getManifestationType() == Manifestations.ManifestationTypes.ALLOMANCY)
-						|| (fullFeruchemist && manifestation.getManifestationType() == Manifestations.ManifestationTypes.FERUCHEMY))
+				//check the entity we are trying to
+				SpiritwebCapability.get((LivingEntity) accessor.getEntity()).ifPresent(targetSpiritweb ->
 				{
-					continue;
-				}
-				tooltip.add(manifestation.translation());
+					//show all manifestations, including hemalurgic based ones.
+					for (AManifestation manifestation : targetSpiritweb.getAvailableManifestations())
+					{
+						if (manifestation.isActive(targetSpiritweb))
+						{
+							continue;
+						}
+						tooltip.add(manifestation.translation());
+					}
+				});
 			}
 		});
 	}
