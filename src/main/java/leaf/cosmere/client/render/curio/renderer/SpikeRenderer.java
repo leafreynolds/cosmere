c93b824738ca33931d0ed8a13b15a726a5dd918c
Leaf <leafreynolds@gmail.com>
2022-06-07 20:59:43 +1200

    Hide cosmere curios when invisible potion effect applied

--- src/main/java/leaf/cosmere/client/render/curio/renderer/SpikeRenderer.java
+++ src/main/java/leaf/cosmere/client/render/curio/renderer/SpikeRenderer.java
@@ -8,6 +8,8 @@ import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import top.theillusivec4.curios.api.SlotContext;
@@ -39,6 +41,12 @@ public class SpikeRenderer implements ICurioRenderer
 	{
 
 		LivingEntity entity = slotContext.entity();
+
+		MobEffectInstance effectInstance = entity.getEffect(MobEffects.INVISIBILITY);
+		if (effectInstance != null && effectInstance.getDuration() > 0)
+		{
+			return;
+		}
 
 		this.model.prepareMobModel(entity, limbSwing, limbSwingAmount, partialTicks);
 		this.model.setupAnim(entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch);
