92a1a075e33ebcadb820c582698fe22bceedc2ed
Leaf <leafreynolds@gmail.com>
2022-06-07 18:53:48 +1200

    Tin store now affects render distance.

--- src/main/java/leaf/cosmere/mixin/GameRendererMixin.java
+++ src/main/java/leaf/cosmere/mixin/GameRendererMixin.java
@@ -5,11 +5,13 @@ import leaf.cosmere.registry.EffectsRegistry;
 import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.util.Mth;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -25,8 +27,7 @@ public class GameRendererMixin
 		info.setReturnValue(i > 0 ? 1 : i);
 	}
 
-	@Inject(
-			at = @At("RETURN"),
+	@Inject(at = @At("RETURN"),
 			method = "getFov",
 			cancellable = true
 	)
@@ -35,18 +36,10 @@ public class GameRendererMixin
 		double fov = info.getReturnValue();
 		Player player = (Player) Minecraft.getInstance().getCameraEntity();
 		MobEffectInstance tinTapEffect = player.getEffect(EffectsRegistry.TAPPING_EFFECTS.get(Metals.MetalType.TIN).get());
-		MobEffectInstance tinStoreEffect = player.getEffect(EffectsRegistry.STORING_EFFECTS.get(Metals.MetalType.TIN).get());
 		if (tinTapEffect != null && tinTapEffect.getDuration() > 0)
 		{
 			int amplifier = tinTapEffect.getAmplifier();
 			double zoomedFov = fov / (amplifier + 2);
-			info.setReturnValue(zoomedFov);
-			return;
-		}
-		if (tinStoreEffect != null && tinStoreEffect.getDuration() > 0)
-		{
-			int amplifier = tinStoreEffect.getAmplifier();
-			double zoomedFov = fov * (amplifier + 2);
 			info.setReturnValue(zoomedFov);
 			return;
 		}
@@ -55,4 +48,29 @@ public class GameRendererMixin
 	}
 
 
+	@Inject(at = @At("RETURN"),
+			method = "getRenderDistance",
+			cancellable = true
+	)
+	private void getRenderDistance(CallbackInfoReturnable<Float> info)
+	{
+		float renderDistance = info.getReturnValue();
+		Player player = (Player) Minecraft.getInstance().getCameraEntity();
+		MobEffectInstance tinTapEffect = null;
+		if (player != null)
+		{
+			tinTapEffect = player.getEffect(EffectsRegistry.STORING_EFFECTS.get(Metals.MetalType.TIN).get());
+		}
+		if (tinTapEffect != null && tinTapEffect.getDuration() > 0)
+		{
+			int amplifier = tinTapEffect.getAmplifier();
+			final int i = amplifier + 1;
+			final double pow = Math.pow(2, i);
+			final int floor = Mth.fastFloor(pow);
+			final float v = renderDistance / floor;
+
+			info.setReturnValue(v);
+		}
+
+	}
 }
