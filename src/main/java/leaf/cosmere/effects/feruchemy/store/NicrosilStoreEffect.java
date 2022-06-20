07c4e7926f3dbe91b7ca50763abbba581b0203ba
Leaf <leafreynolds@gmail.com>
2022-06-20 13:52:11 +1200

    don't disable nicrosil when storing

--- src/main/java/leaf/cosmere/effects/feruchemy/store/NicrosilStoreEffect.java
+++ src/main/java/leaf/cosmere/effects/feruchemy/store/NicrosilStoreEffect.java
@@ -4,22 +4,13 @@
 
 package leaf.cosmere.effects.feruchemy.store;
 
-import leaf.cosmere.cap.entity.SpiritwebCapability;
-import leaf.cosmere.constants.Constants;
 import leaf.cosmere.constants.Metals;
 import leaf.cosmere.effects.feruchemy.FeruchemyEffectBase;
 import leaf.cosmere.manifestation.AManifestation;
-import leaf.cosmere.manifestation.feruchemy.FeruchemyNicrosil;
 import leaf.cosmere.registry.AttributesRegistry;
 import leaf.cosmere.registry.ManifestationRegistry;
-import leaf.cosmere.utils.helpers.CompoundNBTHelper;
 import net.minecraft.world.effect.MobEffectCategory;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.ai.attributes.Attribute;
-import net.minecraft.world.entity.ai.attributes.AttributeMap;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
-import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraftforge.registries.RegistryObject;
 
 
 public class NicrosilStoreEffect extends FeruchemyEffectBase
@@ -31,7 +22,8 @@ public class NicrosilStoreEffect extends FeruchemyEffectBase
 		for (AManifestation manifestation : ManifestationRegistry.MANIFESTATION_REGISTRY.get())
 		{
 			String manifestationName = manifestation.getName();
-			if (!AttributesRegistry.COSMERE_ATTRIBUTES.containsKey(manifestationName))
+			if (!AttributesRegistry.COSMERE_ATTRIBUTES.containsKey(manifestationName)
+					|| manifestation == ManifestationRegistry.FERUCHEMY_POWERS.get(Metals.MetalType.NICROSIL).get())
 			{
 				continue;
 			}
