d0db05ca7da781a18c50628ae57092b40ad5a4f4
Leaf <leafreynolds@gmail.com>
2022-07-03 18:13:09 +1200

    feru cadmium gets minor speed buff for highly oxygenated blood

--- src/main/java/leaf/cosmere/effects/feruchemy/tap/CadmiumTapEffect.java
+++ src/main/java/leaf/cosmere/effects/feruchemy/tap/CadmiumTapEffect.java
@@ -9,6 +9,8 @@ import leaf.cosmere.effects.feruchemy.FeruchemyEffectBase;
 import net.minecraft.util.Mth;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 
 
 public class CadmiumTapEffect extends FeruchemyEffectBase
@@ -16,6 +18,18 @@ public class CadmiumTapEffect extends FeruchemyEffectBase
 	public CadmiumTapEffect(Metals.MetalType type, MobEffectCategory effectType)
 	{
 		super(type, effectType);
+
+		this.addAttributeModifier(
+				Attributes.ATTACK_SPEED,
+				"019c1754-d7a2-4c78-9e14-896ecc7ed0e2",
+				0.01F,
+				AttributeModifier.Operation.MULTIPLY_TOTAL);
+
+		this.addAttributeModifier(
+				Attributes.MOVEMENT_SPEED,
+				"019c1754-d7a2-4c78-9e14-896ecc7ed0e2",
+				0.02F,
+				AttributeModifier.Operation.MULTIPLY_TOTAL);
 	}
 
 	@Override
