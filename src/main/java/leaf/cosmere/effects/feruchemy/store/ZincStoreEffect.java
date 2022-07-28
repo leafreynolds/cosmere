b43d97d86ed3be132d4d9fb510411e39d0d496d5
Leaf <leafreynolds@gmail.com>
2022-07-29 10:04:47 +1200

    f-zinc to 15%, was 10%

--- src/main/java/leaf/cosmere/effects/feruchemy/store/ZincStoreEffect.java
+++ src/main/java/leaf/cosmere/effects/feruchemy/store/ZincStoreEffect.java
@@ -19,8 +19,8 @@ public class ZincStoreEffect extends FeruchemyEffectBase
 		addAttributeModifier(
 				AttributesRegistry.XP_RATE_ATTRIBUTE.get(),
 				"9d50ed05-06f4-4bc2-83bc-d870f93696bf",
-				-0.1D,
-				AttributeModifier.Operation.ADDITION);
+				-0.15D,
+				AttributeModifier.Operation.MULTIPLY_TOTAL);
 	}
 
 }
