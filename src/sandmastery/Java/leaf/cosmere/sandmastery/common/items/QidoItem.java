b2b505defee886d863471ee12895e72f5927c78d
MoonlitJolteon <rockhillnathan@gmail.com>
2022-11-06 13:27:14 -0500

    [Sandmastery] The qido can actually be filled now

--- src/sandmastery/Java/leaf/cosmere/sandmastery/common/items/QidoItem.java
+++ src/sandmastery/Java/leaf/cosmere/sandmastery/common/items/QidoItem.java
@@ -7,13 +7,21 @@ import leaf.cosmere.common.properties.PropTypes;
 import leaf.cosmere.sandmastery.common.capabilities.SandmasterySpiritwebSubmodule;
 import leaf.cosmere.sandmastery.common.itemgroups.SandmasteryItemGroups;
 import leaf.cosmere.sandmastery.common.registries.SandmasteryManifestations;
+import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
 
 public class QidoItem extends ChargeableItemBase {
@@ -29,35 +37,42 @@ public class QidoItem extends ChargeableItemBase {
     @Override
     public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
         ItemStack itemStack = pPlayer.getItemInHand(pUsedHand);
-
         if (pLevel.isClientSide) {
             return InteractionResultHolder.pass(itemStack);
         }
 
-        SpiritwebCapability.get(pPlayer).ifPresent(spiritweb ->
-        {
-            SpiritwebCapability data = (SpiritwebCapability) spiritweb;
-
-            boolean isMaster = SandmasteryManifestations.SANDMASTERY_POWERS.values().stream().anyMatch((manifestation -> spiritweb.hasManifestation(manifestation.getManifestation())));
-
-            if (!isMaster) return;
-
-            final int liquid = getCharge(itemStack);
-
-            SandmasterySpiritwebSubmodule sb = (SandmasterySpiritwebSubmodule) data.spiritwebSubmodules.get(Manifestations.ManifestationTypes.SANDMASTERY);
-
-            int playerHydration = sb.getHydrationLevel();
-            final int maxPlayerHydration = sb.MAX_HYDRATION;
-
-            if (liquid + playerHydration > maxPlayerHydration) {
-                sb.adjustHydration((maxPlayerHydration - playerHydration), true);
-                setCharge(itemStack, ((liquid + playerHydration) - maxPlayerHydration));
-            } else {
-                sb.adjustHydration(liquid, true);
-                setCharge(itemStack, 0);
+        if(pPlayer.isCrouching()) {
+            BlockPos pos = getPlayerPOVHitResult(pLevel, pPlayer, ClipContext.Fluid.WATER).getBlockPos();
+            BlockState state = pLevel.getBlockState(pos);
+            if(state.is(Blocks.WATER)) {
+                pLevel.playSound(pPlayer, pPlayer.getX(), pPlayer.getY(), pPlayer.getZ(), SoundEvents.BOTTLE_FILL, SoundSource.NEUTRAL, 1.0F, 1.0F);
+                setCharge(itemStack, getMaxCharge(itemStack));
             }
-        });
+        } else {
+            SpiritwebCapability.get(pPlayer).ifPresent(spiritweb ->
+            {
+                SpiritwebCapability data = (SpiritwebCapability) spiritweb;
 
+                boolean isMaster = SandmasteryManifestations.SANDMASTERY_POWERS.values().stream().anyMatch((manifestation -> spiritweb.hasManifestation(manifestation.getManifestation())));
+
+                if (!isMaster) return;
+
+                final int liquid = getCharge(itemStack);
+
+                SandmasterySpiritwebSubmodule sb = (SandmasterySpiritwebSubmodule) data.spiritwebSubmodules.get(Manifestations.ManifestationTypes.SANDMASTERY);
+
+                int playerHydration = sb.getHydrationLevel();
+                final int maxPlayerHydration = sb.MAX_HYDRATION;
+
+                if (liquid + playerHydration > maxPlayerHydration) {
+                    sb.adjustHydration((maxPlayerHydration - playerHydration), true);
+                    setCharge(itemStack, ((liquid + playerHydration) - maxPlayerHydration));
+                } else {
+                    sb.adjustHydration(liquid, true);
+                    setCharge(itemStack, 0);
+                }
+            });
+        }
 
         return InteractionResultHolder.consume(itemStack);
     }
