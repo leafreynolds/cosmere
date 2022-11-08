0a67b84f50134d8f648628181b09a55bc9e2df8d
MoonlitJolteon <rockhillnathan@gmail.com>
2022-11-08 02:32:54 -0500

    [Sandmastery] Cushion ability no longer blocks upward movement

--- src/sandmastery/Java/leaf/cosmere/sandmastery/common/manifestation/MasteryCushion.java
+++ src/sandmastery/Java/leaf/cosmere/sandmastery/common/manifestation/MasteryCushion.java
@@ -41,10 +41,12 @@ public class MasteryCushion extends SandmasteryManifestation{
         SandmasterySpiritwebSubmodule submodule = (SandmasterySpiritwebSubmodule) playerSpiritweb.spiritwebSubmodules.get(Manifestations.ManifestationTypes.SANDMASTERY);
 
         LivingEntity living = data.getLiving();
+        Vec3 movement = living.getDeltaMovement();
+        if(movement.y > 0) return;
         double distFromGround = MiscHelper.distanceFromGround(living);
         if(!(distFromGround > 1 && distFromGround < 10)) return;
         if(!submodule.adjustHydration(-10, false)) return;
-        living.setDeltaMovement(living.getDeltaMovement().multiply(1, 0.05, 1));
+        living.setDeltaMovement(movement.multiply(1, 0.05, 1));
         living.hurtMarked = true;
         living.resetFallDistance();
         submodule.adjustHydration(-10, true);
