832cbf9a736e414dc40e465ea00861e9db1d97b3
Leaf <leafreynolds@gmail.com>
2023-01-30 09:16:12 +1300

    highstorm prep only, nothing amazing

--- src/surgebinding/java/leaf/cosmere/surgebinding/common/eventHandlers/SurgebindingCapabilitiesHandler.java
+++ src/surgebinding/java/leaf/cosmere/surgebinding/common/eventHandlers/SurgebindingCapabilitiesHandler.java
@@ -1,5 +1,5 @@
 /*
- * File updated ~ 8 - 10 - 2022 ~ Leaf
+ * File updated ~ 1 - 11 - 2022 ~ Leaf
  */
 
 package leaf.cosmere.surgebinding.common.eventHandlers;
@@ -10,11 +10,13 @@ import leaf.cosmere.surgebinding.common.capability.world.IRoshar;
 import leaf.cosmere.surgebinding.common.capability.world.RosharCapability;
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilitySerializable;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -30,7 +32,7 @@ public class SurgebindingCapabilitiesHandler
 	{
 		Level level = event.getObject();
 
-		if (event.getObject().dimension().location().toString().contains("roshar"))
+		if (level.dimension().location().toString().contains("roshar"))
 		{
 			event.addCapability(Constants.Resources.ROSHAR_CAP, new ICapabilitySerializable<CompoundTag>()
 			{
@@ -60,5 +62,28 @@ public class SurgebindingCapabilitiesHandler
 		}
 
 	}
+
+
+	@SubscribeEvent
+	public void onWorldTick(TickEvent.LevelTickEvent event)
+	{
+		Level level = event.level;
+		if (event.phase == TickEvent.Phase.END && event.side.isServer())
+		{
+			MinecraftServer server = level.getServer();
+			if (server != null && server.getPlayerList().getPlayerCount() == 0)
+			{
+				return;
+			}
+
+			RosharCapability.get(level).ifPresent(iRoshar ->
+			{
+
+			});
+
+		}
+
+	}
+
 
 }
