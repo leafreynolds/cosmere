27fbd3866b6082bf99cf687454f2f15b89b77dc0
Leaf <leafreynolds@gmail.com>
2023-01-30 20:12:03 +1300

    [Cosmere] Fix metal trader not finding profession properly

--- src/hemalurgy/java/leaf/cosmere/hemalurgy/client/render/model/KolossModel.java
+++ src/hemalurgy/java/leaf/cosmere/hemalurgy/client/render/model/KolossModel.java
@@ -11,17 +11,13 @@ package leaf.cosmere.hemalurgy.client.render.model;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.model.EntityModel;
-import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.*;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 
 public class KolossModel<T extends Entity> extends EntityModel<T>
 {
-	// This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
-	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("modid", "koloss"), "main");
 	private final ModelPart bb_main;
 
 	public KolossModel(ModelPart root)
