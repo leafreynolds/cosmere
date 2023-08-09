eacf1e2af7ae7a9b08bff15fed1245987f64289f
Eliot Lash <fadookie@gmail.com>
2023-08-09 03:33:11 +0000

    [Surgebinding] Make Lavis Polyp a 4-way directional block

--- src/surgebinding/java/leaf/cosmere/surgebinding/common/blocks/LavisPolypBlock.java
+++ src/surgebinding/java/leaf/cosmere/surgebinding/common/blocks/LavisPolypBlock.java
@@ -1,10 +1,14 @@
 package leaf.cosmere.surgebinding.common.blocks;
 
-import leaf.cosmere.common.blocks.BaseBlock;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
 import net.minecraft.world.phys.shapes.CollisionContext;
@@ -12,7 +16,8 @@ import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 
-public class LavisPolypBlock extends BaseBlock {
+public class LavisPolypBlock extends HorizontalDirectionalBlock  {
+    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
     public static final VoxelShape SHAPE;
 
     public LavisPolypBlock () {
@@ -22,6 +27,14 @@ public class LavisPolypBlock extends BaseBlock {
     @Override
     public @NotNull VoxelShape getShape(@NotNull BlockState pState, @NotNull BlockGetter pLevel, @NotNull BlockPos pPos, @NotNull CollisionContext pContext) {
         return SHAPE;
+    }
+
+    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+        return this.defaultBlockState().setValue(FACING, pContext.getHorizontalDirection().getOpposite());
+    }
+
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(FACING);
     }
 
     static {
