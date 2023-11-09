bd77a3a41755f11786c7a073becdb86b6811442c
X66Herobrine66X <31418501+X66Herobrine66X@users.noreply.github.com>
2023-11-08 20:12:42 -0500

    Update CuriosCompat.java

--- src/main/java/leaf/cosmere/common/compat/curios/CuriosCompat.java
+++ src/main/java/leaf/cosmere/common/compat/curios/CuriosCompat.java
@@ -4,7 +4,7 @@
 
 package leaf.cosmere.common.compat.curios;
 
-import net.minecraft.world.inventory.InventoryMenu;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.ModList;
@@ -16,6 +16,8 @@ import top.theillusivec4.curios.api.SlotTypePreset;
 
 public class CuriosCompat
 {
+
+	public static final ResourceLocation EMPTY_SPIKE_SLOT = new ResourceLocation("curios:slot/spike_icon");
 	private static boolean curiosModDetected;
 
 
@@ -45,35 +47,51 @@ public class CuriosCompat
 			return;
 		}
 
-		SlotTypePreset[] oneSlot = {
-				SlotTypePreset.BACK,
-				SlotTypePreset.BELT,
-		};
-
-		for (SlotTypePreset type : oneSlot)
-		{
-			InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> type.getMessageBuilder().build());
-		}
-
-		SlotTypePreset[] twoSlot = {
-				SlotTypePreset.HEAD,
-				SlotTypePreset.HANDS,
-				SlotTypePreset.CHARM,
-				SlotTypePreset.NECKLACE,
-		};
-
-		for (SlotTypePreset type : twoSlot)
-		{
-			InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> type.getMessageBuilder().size(2).build());
-		}
-		InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> SlotTypePreset.BRACELET.getMessageBuilder().size(4).build());
-		InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> SlotTypePreset.BODY.getMessageBuilder().size(8).build());
+		InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> SlotTypePreset.NECKLACE.getMessageBuilder().size(2).build());
 		InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> SlotTypePreset.RING.getMessageBuilder().size(8).build());
 
 		//custom slots
-		InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> new SlotTypeMessage.Builder("legs").priority(450).size(2).icon(InventoryMenu.EMPTY_ARMOR_SLOT_LEGGINGS).build());
-		InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> new SlotTypeMessage.Builder("feet").priority(500).size(2).icon(InventoryMenu.EMPTY_ARMOR_SLOT_BOOTS).build());
-		InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> new SlotTypeMessage.Builder("linchpin").priority(59).size(1).icon(InventoryMenu.EMPTY_ARMOR_SLOT_HELMET).build());
+		InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> new SlotTypeMessage.Builder("eyes").priority(0).size(2).icon(EMPTY_SPIKE_SLOT).build());
+		// Physical 2
+		InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> new SlotTypeMessage.Builder("linchpin").priority(1).size(1).icon(EMPTY_SPIKE_SLOT).build());
+		// Physical 1
+		InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> new SlotTypeMessage.Builder("back").priority(2).size(4).icon(EMPTY_SPIKE_SLOT).build());
+		// Mental 4
+		InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> new SlotTypeMessage.Builder("chest").priority(3).size(2).icon(EMPTY_SPIKE_SLOT).build());
+		// Physical 2
+		InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> new SlotTypeMessage.Builder("shoulders_top").priority(4).size(2).icon(EMPTY_SPIKE_SLOT).build());
+		// Physical 2
+		InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> new SlotTypeMessage.Builder("shoulders_back").priority(5).size(2).icon(EMPTY_SPIKE_SLOT).build());
+		// Temporal 2
+		InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> new SlotTypeMessage.Builder("upper_arms_back").priority(6).size(2).icon(EMPTY_SPIKE_SLOT).build());
+		// Temporal 2
+		InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> new SlotTypeMessage.Builder("shoulders_side").priority(7).size(2).icon(EMPTY_SPIKE_SLOT).build());
+		// Spiritual 2
+		InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> new SlotTypeMessage.Builder("upper_arms").priority(8).size(2).icon(EMPTY_SPIKE_SLOT).build());
+		// Mental 2
+		InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> new SlotTypeMessage.Builder("middle_arms").priority(9).size(2).icon(EMPTY_SPIKE_SLOT).build());
+		// Temporal 2
+		InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> new SlotTypeMessage.Builder("lower_arms").priority(10).size(2).icon(EMPTY_SPIKE_SLOT).build());
+		// Spiritual 2
+		InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> new SlotTypeMessage.Builder("hands").priority(11).size(2).icon(EMPTY_SPIKE_SLOT).build());
+		// Temporal 2
+		InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> new SlotTypeMessage.Builder("ribs_top").priority(12).size(2).icon(EMPTY_SPIKE_SLOT).build());
+		// Physical 2
+		InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> new SlotTypeMessage.Builder("ribs_top_middle").priority(13).size(2).icon(EMPTY_SPIKE_SLOT).build());
+		// Mental 2
+		InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> new SlotTypeMessage.Builder("ribs_bottom_middle").priority(14).size(2).icon(EMPTY_SPIKE_SLOT).build());
+		// Temporal 2
+		InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> new SlotTypeMessage.Builder("ribs_bottom").priority(15).size(2).icon(EMPTY_SPIKE_SLOT).build());
+		// Spiritual 2
+		InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> new SlotTypeMessage.Builder("upper_legs").priority(16).size(2).icon(EMPTY_SPIKE_SLOT).build());
+		// Physical 2
+		InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> new SlotTypeMessage.Builder("upper_middle_legs").priority(17).size(2).icon(EMPTY_SPIKE_SLOT).build());
+		// Mental 2
+		InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> new SlotTypeMessage.Builder("lower_middle_legs").priority(18).size(2).icon(EMPTY_SPIKE_SLOT).build());
+		// Temporal 2
+		InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> new SlotTypeMessage.Builder("lower_legs").priority(19).size(2).icon(EMPTY_SPIKE_SLOT).build());
+		// Spiritual 2
+
 
 	}
 }
