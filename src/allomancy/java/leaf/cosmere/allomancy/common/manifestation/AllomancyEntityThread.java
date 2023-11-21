cce18d3374c6fd236768944bc18ed14b7cb5ce74
Gerbagel <leongroth98@gmail.com>
2023-11-21 16:40:46 +0100

    [Allomancy] Entity thread basic refactor.

--- src/allomancy/java/leaf/cosmere/allomancy/common/manifestation/AllomancyEntityThread.java
+++ src/allomancy/java/leaf/cosmere/allomancy/common/manifestation/AllomancyEntityThread.java
@@ -4,13 +4,20 @@
 
 package leaf.cosmere.allomancy.common.manifestation;
 
+import leaf.cosmere.api.CosmereAPI;
 import leaf.cosmere.api.spiritweb.ISpiritweb;
+import net.minecraft.world.entity.LivingEntity;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class AllomancyEntityThread implements Runnable
 {
+	private final List<LivingEntity> entityList = new ArrayList<>();
+	private final Lock localLock = new ReentrantLock();
 	public static boolean serverShutdown = false;
 	public static final Lock lock = new ReentrantLock();
 	public final ISpiritweb data;
@@ -19,6 +26,33 @@ public class AllomancyEntityThread implements Runnable
 	public AllomancyEntityThread(ISpiritweb data)
 	{
 		this.data = data;
+	}
+
+	public List<LivingEntity> requestEntityList()
+	{
+		localLock.lock();
+		try
+		{
+			return entityList;
+		}
+		catch (Exception e)
+		{
+			CosmereAPI.logger.info(Arrays.toString(e.getStackTrace()));
+		}
+
+		// empty return in case of exception
+		return new ArrayList<>();
+	}
+
+	public void releaseEntityList()
+	{
+		localLock.unlock();
+	}
+
+	public void setEntityList(List<LivingEntity> newList)
+	{
+		entityList.clear();
+		entityList.addAll(newList);
 	}
 
 	@Override
