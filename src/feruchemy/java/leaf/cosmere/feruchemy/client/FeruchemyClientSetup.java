4c57df0febc1f07f9df982b89f5400e93b9d0228
Roseplate <66807484+Roseplate@users.noreply.github.com>
2025-11-11 01:31:26 +0000

    Early Nicrosil Changes. Mostly menu stuff.

--- src/feruchemy/java/leaf/cosmere/feruchemy/client/FeruchemyClientSetup.java
+++ src/feruchemy/java/leaf/cosmere/feruchemy/client/FeruchemyClientSetup.java
@@ -5,12 +5,18 @@
 package leaf.cosmere.feruchemy.client;
 
 import leaf.cosmere.api.CosmereAPI;
+import leaf.cosmere.common.cap.entity.SpiritwebCapability;
+import leaf.cosmere.feruchemy.client.gui.NicrosilMenu;
 import leaf.cosmere.feruchemy.client.render.FeruchemyLayerDefinitions;
 import leaf.cosmere.feruchemy.client.render.FeruchemyRenderers;
 import leaf.cosmere.feruchemy.client.render.model.BraceletModel;
 import leaf.cosmere.feruchemy.common.Feruchemy;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
+import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -32,6 +38,29 @@ public class FeruchemyClientSetup
 		FeruchemyRenderers.register();
 
 		CosmereAPI.logger.info("Feruchemy client setup complete!");
+	}
+
+	@SubscribeEvent
+	public static void registerGuiOverlays(RegisterGuiOverlaysEvent event)
+	{
+		event.registerBelow(
+				VanillaGuiOverlay.DEBUG_TEXT.id(),
+				"hud",
+				(gui, guiGraphics, partialTick, width, height) -> renderNicrosilHUD(guiGraphics)
+		);
+	}
+
+	public static void renderNicrosilHUD(final GuiGraphics guiGraphics)
+	{
+		final Minecraft mc = Minecraft.getInstance();
+		SpiritwebCapability.get(mc.player).ifPresent(cap ->
+		{
+			//SpiritwebCapability spiritweb = (SpiritwebCapability) cap;
+
+			//actual menu stuff
+			NicrosilMenu.instance.postRender();
+		});
+
 	}
 
 }
