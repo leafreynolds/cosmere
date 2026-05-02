c31f06ca646303335069800653ac5360f57aa4b4
Gerbagel <leongroth98@gmail.com>
2026-05-02 02:26:40 +0200

    [Surgebinding] Added entire menu

--- src/surgebinding/java/leaf/cosmere/surgebinding/client/gui/CircleButton.java
+++ src/surgebinding/java/leaf/cosmere/surgebinding/client/gui/CircleButton.java
@@ -1,4 +1,4 @@
-package leaf.cosmere.sandmastery.client.gui;
+package leaf.cosmere.surgebinding.client.gui;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.BufferBuilder;
@@ -6,74 +6,48 @@ import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import com.mojang.blaze3d.vertex.Tesselator;
 import com.mojang.blaze3d.vertex.VertexFormat;
 import leaf.cosmere.api.manifestation.Manifestation;
+import leaf.cosmere.api.spiritweb.ISpiritweb;
 import leaf.cosmere.client.gui.SpiritwebMenu;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.resources.language.I18n;
 import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.resources.ResourceLocation;
 import org.joml.Matrix4f;
 import org.lwjgl.opengl.GL11;
 
-public class RadialButton extends Button
+public class CircleButton extends Button
 {
-	private final int radius, centerX, centerY, segmentNr;
-	private final float startAngle;
-	private final float endAngle;
-	private final Manifestation manifestation;
-	protected RadialButton(int pX, int pY, int radius, int segmentNr, Manifestation manifestation)
-	{
-		super(pX, pY, 16, 16, CommonComponents.EMPTY, (button) -> { }, DEFAULT_NARRATION);
-		this.manifestation = manifestation;
-		this.radius = radius;
-		this.segmentNr = segmentNr;
-		centerX = pX;
-		centerY = pY;
+	final ISpiritweb spiritweb;
+	final Manifestation manifestation;
+	final int centerX, centerY;
+	final int radius;
 
-		float fifthCircle = (float) Math.toRadians(360d/5d);
-		startAngle = (float) (fifthCircle*segmentNr);
-		endAngle = startAngle + fifthCircle;
+	protected CircleButton(int pX, int pY, int radius, ISpiritweb spiritweb, Manifestation manifestation)
+	{
+		super(pX, pY, radius, radius, CommonComponents.EMPTY, (button) -> { }, DEFAULT_NARRATION);
+
+		this.spiritweb = spiritweb;
+		this.centerX = pX;
+		this.centerY = pY;
+		this.radius = radius;
+		this.manifestation = manifestation;
 	}
 
 	@Override
-	public boolean isMouseOver(double mouseX, double mouseY)
-	{
-		boolean retVal;
+	public boolean isMouseOver(double mouseX, double mouseY) {
 		double distanceX = mouseX - centerX;
 		double distanceY = mouseY - centerY;
-		double dSqr = distanceX * distanceX + distanceY * distanceY;
 
-		if (dSqr > radius * radius)
-		{
-			return false;
-		}
-
-		double angle = Math.atan2(distanceY, distanceX);
-
-		if (angle < 0) {
-			angle += 2 * Math.PI;
-		}
-
-		double start = normalizeAngle(startAngle);
-		double end = normalizeAngle(endAngle);
-		angle = normalizeAngle(angle);
-
-		if (start <= end)
-		{
-			retVal = angle >= start && angle <= end;
-		}
-		else
-		{
-			retVal = angle >= start || angle <= end;
-		}
-
-		if (retVal)
-		{
+		if ((distanceX * distanceX + distanceY * distanceY) <= (radius * radius)) {
 			SpiritwebMenu.selectedManifestation = manifestation;
+			return true;
 		}
 
-
-		return retVal;
+		return false;
 	}
 
 	@Override
@@ -86,26 +60,24 @@ public class RadialButton extends Button
 	public void render(GuiGraphics pGuiGraphics, int pMouseX, int pMouseY, float pPartialTick)
 	{
 		boolean isHovered = isMouseOver(pMouseX, pMouseY);
-		renderRadial(pGuiGraphics, isHovered);
+		renderCircle(pGuiGraphics, isHovered);
 		renderIcon(pGuiGraphics);
+		if (isHovered)
+			renderText(pGuiGraphics);
 	}
 
-	private void renderRadial(GuiGraphics pGuiGraphics, boolean isHovered)
+	private void renderCircle(GuiGraphics pGuiGraphics, boolean isHovered)
 	{
 		float r = 61/255.f, g = 70/255.f, b = 76/255.f;
 		float a = 1f;
 
-		if (isHovered)
-		{
+		if (isHovered) {
 			r *= 1.1f;
 			g *= 1.1f;
 			b *= 1.1f;
 		}
 
-		float radsPerSegment = (float) Math.PI * 2 / 5;
-		float startAngle = segmentNr * radsPerSegment;
 		float radiusSq = radius * radius;
-
 		int pixelSize = 1;
 
 		RenderSystem.disableCull();
@@ -119,33 +91,19 @@ public class RadialButton extends Button
 
 		buf.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_COLOR);
 
-		for (int x = -radius; x <= radius; x += pixelSize)
-		{
-			for (int y = -radius; y <= radius; y += pixelSize)
-			{
+		for (int x = -radius; x <= radius; x += pixelSize) {
+			for (int y = -radius; y <= radius; y += pixelSize) {
 				float pixelCenterX = x + (pixelSize / 2f);
 				float pixelCenterY = y + (pixelSize / 2f);
 
-				float distSq = pixelCenterX * pixelCenterX + pixelCenterY * pixelCenterY;
+				if ((pixelCenterX * pixelCenterX + pixelCenterY * pixelCenterY) <= radiusSq) {
+					float px = centerX + x;
+					float py = centerY + y;
 
-				if (distSq <= radiusSq)
-				{
-					float angle = (float) Math.atan2(pixelCenterY, pixelCenterX);
-					if (angle < 0) angle += (float) (Math.PI * 2);
-
-					float diff = angle - startAngle;
-					if (diff < 0) diff += (float) (Math.PI * 2);
-
-					if (diff < radsPerSegment)
-					{
-						float px = centerX + x;
-						float py = centerY + y;
-
-						buf.vertex(pose, px, py, 0).color(r, g, b, a).endVertex();
-						buf.vertex(pose, px, py + pixelSize, 0).color(r, g, b, a).endVertex();
-						buf.vertex(pose, px + pixelSize, py + pixelSize, 0).color(r, g, b, a).endVertex();
-						buf.vertex(pose, px + pixelSize, py, 0).color(r, g, b, a).endVertex();
-					}
+					buf.vertex(pose, px, py, 0).color(r, g, b, a).endVertex();
+					buf.vertex(pose, px, py + pixelSize, 0).color(r, g, b, a).endVertex();
+					buf.vertex(pose, px + pixelSize, py + pixelSize, 0).color(r, g, b, a).endVertex();
+					buf.vertex(pose, px + pixelSize, py, 0).color(r, g, b, a).endVertex();
 				}
 			}
 		}
@@ -154,13 +112,16 @@ public class RadialButton extends Button
 		RenderSystem.disableBlend();
 	}
 
-	private void renderIcon(GuiGraphics pGuiGraphics)
+	public void renderIcon(GuiGraphics pGuiGraphics)
 	{
 		StringBuilder stringBuilder = new StringBuilder();
 		stringBuilder.setLength(0);
 		stringBuilder.append("textures/icon/")
 				.append(manifestation.getManifestationType().getName())
-				.append("/.png");
+				.append("/");
+
+		stringBuilder.append(manifestation.getName());
+		stringBuilder.append(".png");
 
 		final ResourceLocation location = new ResourceLocation(manifestation.getRegistryName().getNamespace(), stringBuilder.toString());
 		float alpha = 1.0f;
@@ -168,11 +129,9 @@ public class RadialButton extends Button
 		RenderSystem.enableBlend();
 		RenderSystem.blendFuncSeparate(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, 1, 0);
 
-		double midAngle = (startAngle + endAngle) / 2;
-		double midRadius = (radius) / 1.5;
 		int iconSize = width-2;
-		int posX = centerX + (int)(Math.cos(midAngle) * midRadius) - iconSize/2;
-		int posY = centerY + (int)(Math.sin(midAngle) * midRadius) - iconSize/2;
+		int posX = centerX - iconSize/2;
+		int posY = centerY - iconSize/2;
 
 		RenderSystem.setShaderColor(0f, 0f, 0f, alpha);
 		pGuiGraphics.blit(location,
@@ -203,16 +162,23 @@ public class RadialButton extends Button
 		RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
 	}
 
-	private double normalizeAngle(double angle)
+	private void renderText(GuiGraphics pGuiGraphics)
 	{
-		if (angle != 0)
+		Font font = Minecraft.getInstance().font;
+		String text = I18n.get(manifestation.getTranslationKey());
+		int x;
+		int y = getY();
+		int windowWidth = Minecraft.getInstance().getWindow().getGuiScaledWidth();
+
+		if (getX() < windowWidth/2)
 		{
-			angle = angle % (2 * Math.PI);
-			if (angle < 0)
-			{
-				angle += 2 * Math.PI;
-			}
+			x = getX() - radius - 5 - font.width(text);
 		}
-		return angle;
+		else
+		{
+			x = getX() + radius + 5;
+		}
+
+		pGuiGraphics.drawString(font, text, x, y-font.lineHeight/2, 0xFFFFFFFF);
 	}
 }
