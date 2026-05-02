6e071d3f548f37f3257968d3a0d14cad622d571e
Gerbagel <leongroth98@gmail.com>
2026-05-02 19:29:43 +0200

    [Cosmere] De-green the background color

--- src/main/java/leaf/cosmere/client/gui/GuiUtils.java
+++ src/main/java/leaf/cosmere/client/gui/GuiUtils.java
@@ -4,7 +4,7 @@ import java.awt.*;
 
 public class GuiUtils
 {
-	public static final Color BACKGROUND_COLOR = new Color(61, 70, 76);
+	public static final Color BACKGROUND_COLOR = new Color(61, 61, 71);
 
 	public record CachedQuad(float px, float py, float size) {}
 }
