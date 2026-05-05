bf55ba1f1c9eb8fda27a53bb362eaedaba2fc72a
Gerbagel <leongroth98@gmail.com>
2026-05-05 19:47:21 +0200

    [Allomancy] Display damage delayed by pewter in spiritweb menu

--- src/allomancy/java/leaf/cosmere/allomancy/client/gui/AllomancyInfoBlock.java
+++ src/allomancy/java/leaf/cosmere/allomancy/client/gui/AllomancyInfoBlock.java
@@ -1,6 +1,12 @@
 package leaf.cosmere.allomancy.client.gui;
 
 import com.mojang.blaze3d.systems.RenderSystem;
+import leaf.cosmere.allomancy.common.capabilities.AllomancySpiritwebSubmodule;
+import leaf.cosmere.allomancy.common.manifestation.AllomancyManifestation;
+import leaf.cosmere.allomancy.common.manifestation.AllomancyPewter;
+import leaf.cosmere.api.CosmereAPI;
+import leaf.cosmere.api.IHasMetalType;
+import leaf.cosmere.api.Metals;
 import leaf.cosmere.api.manifestation.Manifestation;
 import leaf.cosmere.api.spiritweb.ISpiritweb;
 import leaf.cosmere.client.gui.GuiUtils;
@@ -16,9 +22,9 @@ import net.minecraft.network.chat.Component;
 public class AllomancyInfoBlock extends AbstractWidget
 {
 	private final ISpiritweb spiritweb;
-	private final Manifestation manifestation;
+	private final AllomancyManifestation manifestation;
 
-	public AllomancyInfoBlock(int pX, int pY, int pWidth, int pHeight, ISpiritweb spiritweb, Manifestation manifestation)
+	public AllomancyInfoBlock(int pX, int pY, int pWidth, int pHeight, ISpiritweb spiritweb, AllomancyManifestation manifestation)
 	{
 		super(pX, pY, pWidth, pHeight, CommonComponents.EMPTY);
 
@@ -30,10 +36,20 @@ public class AllomancyInfoBlock extends AbstractWidget
 	protected void renderWidget(GuiGraphics pGuiGraphics, int pMouseX, int pMouseY, float pPartialTick)
 	{
 		Font font = Minecraft.getInstance().font;
-		int x = getX();
+		float delayedDamagePewter = 0.f;
+		float scale = 0.8f;
+		int x = getX()+5;
 		int y = getY();
 		int width = getWidth();
 		int height = getHeight();
+		if (manifestation.getMetalType() == Metals.MetalType.PEWTER)
+		{
+			delayedDamagePewter = AllomancySpiritwebSubmodule.getSubmodule(spiritweb).getPewterDelayedDamage();
+			if (delayedDamagePewter > 0)
+			{
+				height += font.lineHeight;
+			}
+		}
 		int color = 0x99333333;
 
 		RenderSystem.disableCull();
@@ -43,8 +59,13 @@ public class AllomancyInfoBlock extends AbstractWidget
 		pGuiGraphics.fill(x, y, x + width,  y + height, color);
 
 		String text = I18n.get(manifestation.getTranslationKey());
-		float scale = 0.8f;
-		GuiUtils.drawScaledString(font, pGuiGraphics, text, x+5, y+10, scale, 0xFFFFFFFF);
+		GuiUtils.drawScaledString(font, pGuiGraphics, text, x, y+10, scale, 0xFFFFFFFF);
+
+		if (delayedDamagePewter > 0)
+		{
+			String delayedDamageText = String.format("Delayed damage: %.1f", delayedDamagePewter);
+			GuiUtils.drawScaledString(font, pGuiGraphics, delayedDamageText, x, y+12+font.lineHeight*2, scale, 0xFFFFFFFF);
+		}
 
 		int seconds = manifestation.getInvestitureRemaining(spiritweb);
 		int hours = seconds / 3600;
@@ -60,7 +81,7 @@ public class AllomancyInfoBlock extends AbstractWidget
 		else
 			text = "Empty";
 
-		GuiUtils.drawScaledString(font, pGuiGraphics, text, x+5, y+12+font.lineHeight*scale, scale, 0xFFFFFFFF);
+		GuiUtils.drawScaledString(font, pGuiGraphics, text, x, y+12+font.lineHeight*scale, scale, 0xFFFFFFFF);
 	}
 
 	@Override
