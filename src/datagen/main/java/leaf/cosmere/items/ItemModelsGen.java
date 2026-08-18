c7b00d44e42506820eb9bd8d564cfd8b3e116fde
Gerbagel <leongroth98@gmail.com>
2026-08-18 20:44:56 +0200

    [Port] Port main/items datagen

--- src/datagen/main/java/leaf/cosmere/items/ItemModelsGen.java
+++ src/datagen/main/java/leaf/cosmere/items/ItemModelsGen.java
@@ -14,10 +14,10 @@ import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
-import net.minecraftforge.client.model.generators.ItemModelBuilder;
-import net.minecraftforge.client.model.generators.ItemModelProvider;
-import net.minecraftforge.client.model.generators.ModelFile;
-import net.minecraftforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.client.model.generators.ItemModelBuilder;
+import net.neoforged.neoforge.client.model.generators.ItemModelProvider;
+import net.neoforged.neoforge.client.model.generators.ModelFile;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
 
 import java.util.function.Supplier;
 
